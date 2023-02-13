1c0b884283a7d487951b6c7ba62f002b2f192670
yotam-halperin <yotamhalperin7@gmail.com>
2023-02-13 12:39:35 +0200

    working game with curl

--- app/crossy_road/crossy_main.py
+++ app/crossy_road/crossy_main.py
@@ -297,7 +297,7 @@ def curl_to_host():
     username = f2_text.get()
     print(IP, username, score)
     try:
-        os.popen(f"curl -X POST 'https://{IP}/add_score?user={username}&score={score}' -k")
+        os.system(f"curl -X POST 'https://{IP}/add_score?user={username}&score={score}' --insecure")
     except:
         pass
     first_window.destroy()
